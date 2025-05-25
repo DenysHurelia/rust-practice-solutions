7f74e4e68c66c005465b19538c1794b1cc760516
Denys Hurelia <denys.hurelia@e-u.edu.ua>
2025-05-25 22:53:03 +0300

    Add HackerRank: Simple Array Sum

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,31 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+//- https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+
+fn simpleArraySum(ar: &[i32]) -> i32 {
+    let mut total = 0;
+    for &num in ar {
+        total += num;
+    }
+    total
+}
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+    let _n: i32 = input.trim().parse().unwrap();
+
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+    
+    let arr: Vec<i32> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    
+    println!("{}", simpleArraySum(&arr));
 }
