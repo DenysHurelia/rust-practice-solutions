73aacdc786c77d91ca68804c9ab61ebb21a255c5
Denys Hurelia <denys.hurelia@e-u.edu.ua>
2025-05-26 22:38:37 +0300

    Практична робота #7

---
+++ src/homework/hw07.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let original = String::from("Hello, Привет!");
+    let inverted = invert_the_case(original.clone());
+
+    println!("Original: {}", original);
+    println!("Inverted: {}", inverted);
+}
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().collect::<String>()
+            } else if c.is_uppercase() {
+                c.to_lowercase().collect::<String>()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_invert_case() {
+        let data = [
+            ("Hello", "hELLO"),
+            ("Привет", "пРИВЕТ"),
+        ];
+
+        data.iter().for_each(|(a, b)| {
+            assert_eq!(invert_the_case(a.to_string()), b.to_string());
+            assert_eq!(invert_the_case(b.to_string()), a.to_string());
+        });
+    }
+}
